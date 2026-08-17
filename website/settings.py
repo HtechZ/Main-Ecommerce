ee2282bb43b24630643fc6792319b994d2800784
HtechZ <wobder49@gmail.com>
2026-08-17 16:51:25 +0330

    Second Commit

--- website/settings.py
+++ website/settings.py
@@ -10,6 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
+import cloudinary
+import cloudinary_storage
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
